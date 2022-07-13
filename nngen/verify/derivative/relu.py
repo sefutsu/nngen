0ea360017db5675dcc72f11f2cc20ef0725b2581
sefutsu <sefutsu.r@gmail.com>
2022-07-13 22:48:33 +0900

    refresh style

--- nngen/verify/derivative/relu.py
+++ nngen/verify/derivative/relu.py
@@ -4,18 +4,13 @@ from __future__ import division
 
 import numpy as np
 
-def relu(propagated_gradient, features, dtype=None,
-         features_dtype=None):
+def relu(propagated_gradient, features, dtype=None, pg_dtype=None):
 
-    out_point = 0 if dtype is None else dtype.point
-
-    comp = features > 0
+    rshift_out = 0 if dtype is None or pg_dtype is None else pg_dtype.point - dtype.point
+    propagated_gradient = propagated_gradient >> rshift_out if rshift_out >= 0 \
+                        else propagated_gradient << (-rshift_out)
 
     zeros = np.zeros_like(features, dtype=np.int64)
-    ones = np.ones_like(features, dtype=np.int64) << out_point
-
-    res = np.where(comp, ones, zeros)
-
-    propagated_gradient = propagated_gradient * res
+    propagated_gradient = np.where(features > 0, propagated_gradient, zeros)
     
     return propagated_gradient
