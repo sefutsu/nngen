b7273fdfe9a59e612efc1e7259a9da7b9a65c0cb
sefutsu <sefutsu.r@gmail.com>
2022-10-05 16:48:50 +0900

    pytest for double layer

--- tests/derivative/double_layer.py
+++ tests/derivative/double_layer.py
@@ -1,73 +1,65 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-import sys
-import os
-sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
-
 import nngen as ng
 import numpy as np
 import torch
 
-### nngen.verify
+def generate_inputs():
+    return [
+        np.random.randint(-100, 100, size=(2, 3), dtype=np.int64)
+    ]
 
-dtype = ng.int64
+def generate_weights():
+    return {
+        "w0": np.random.randint(-100, 100, size=(2, 3), dtype=np.int64),
+        "w1": np.random.randint(-100, 100, size=(3, 2), dtype=np.int64),
+        "b0": np.random.randint(-100, 100, size=(2,), dtype=np.int64),
+        "b1": np.random.randint(-100, 100, size=(3,), dtype=np.int64),
+        "scale0": np.random.randint(-100, 100, size=(2,), dtype=np.int64),
+        "scale1": np.random.randint(-100, 100, size=(3,), dtype=np.int64),
+    }
 
-input_layer = ng.placeholder(dtype=dtype, shape=(2, 3), name="input_layer")
-w0 = ng.variable(dtype=dtype, shape=(2, 3), name="w0")
-b0 = ng.variable(dtype=dtype, shape=(2,), name="b0")
-scale0 = ng.variable(dtype=dtype, shape=(2,), name="scale0")
-s0 = ng.matmul(input_layer, w0, bias=b0, transposed_b=True, scale=scale0, act_func=ng.relu, dtype=dtype)
+def nngen_gradient(inputs, weights):
+    dtype = ng.int32
 
-w1 = ng.variable(dtype=dtype, shape=(3, 2), name="w1")
-b1 = ng.variable(dtype=dtype, shape=(3,), name="b1")
-scale1 = ng.variable(dtype=dtype, shape=(3,), name="scale1")
-s1 = ng.matmul(s0, w1, bias=b1, transposed_b=True, scale=scale1, act_func=ng.relu, dtype=dtype)
+    input_layer = ng.placeholder(dtype=dtype, shape=(2, 3), name="input_layer")
+    w0 = ng.variable(dtype=dtype, shape=(2, 3), name="w0")
+    b0 = ng.variable(dtype=dtype, shape=(2,), name="b0")
+    scale0 = ng.variable(dtype=dtype, shape=(2,), name="scale0")
+    s0 = ng.matmul(input_layer, w0, bias=b0, transposed_b=True, scale=scale0, act_func=ng.relu, dtype=dtype)
 
-w0_value = np.random.randint(-100, 100, size=(2, 3), dtype=np.int64)
-w1_value = np.random.randint(-100, 100, size=(3, 2), dtype=np.int64)
-w0.set_value(w0_value)
-w1.set_value(w1_value)
+    w1 = ng.variable(dtype=dtype, shape=(3, 2), name="w1")
+    b1 = ng.variable(dtype=dtype, shape=(3,), name="b1")
+    scale1 = ng.variable(dtype=dtype, shape=(3,), name="scale1")
+    s1 = ng.matmul(s0, w1, bias=b1, transposed_b=True, scale=scale1, act_func=ng.relu, dtype=dtype)
 
-b0_value = np.random.randint(-100, 100, size=(2,), dtype=np.int64)
-b1_value = np.random.randint(-100, 100, size=(3,), dtype=np.int64)
-b0.set_value(b0_value)
-b1.set_value(b1_value)
+    w0.set_value(weights["w0"])
+    w1.set_value(weights["w1"])
+    b0.set_value(weights["b0"])
+    b1.set_value(weights["b1"])
+    scale0.set_value(weights["scale0"])
+    scale1.set_value(weights["scale1"])
 
-scale0_value = np.random.randint(0, 100, size=(2,), dtype=np.int64)
-scale1_value = np.random.randint(0, 100, size=(3,), dtype=np.int64)
-scale0.set_value(scale0_value)
-scale1.set_value(scale1_value)
+    eval_res = ng.eval([s1], input_layer=inputs[0])
+    return ng.gradient(s1, input_layer)
 
-input_value = np.random.randint(-100, 100, size=(2, 3), dtype=np.int64)
 
-eval_res = ng.eval([s1], input_layer=input_value)
-res = ng.gradient(s1, input_layer)
+def torch_gradient(inputs, weights):
+    dtype = torch.double
 
-### torch.autograd
+    input_value = torch.tensor(inputs[0], dtype=dtype, requires_grad=True)
+    w0 = torch.tensor(weights["w0"], dtype=dtype)
+    b0 = torch.tensor(weights["b0"], dtype=dtype)
+    w1 = torch.tensor(weights["w1"], dtype=dtype)
+    b1 = torch.tensor(weights["b1"], dtype=dtype)
+    scale0 = torch.tensor(weights["scale0"], dtype=dtype)
+    scale1 = torch.tensor(weights["scale1"], dtype=dtype)
 
-dtype = torch.double
+    s0 = torch.relu((input_value @ w0.T + b0) * scale0)
+    s1 = torch.relu((s0 @ w1.T + b1) * scale1)
+    s = s1.sum()
 
-input_value = torch.tensor(input_value, dtype=dtype, requires_grad=True)
-w0 = torch.tensor(w0_value, dtype=dtype)
-b0 = torch.tensor(b0_value, dtype=dtype)
-w1 = torch.tensor(w1_value, dtype=dtype)
-b1 = torch.tensor(b1_value, dtype=dtype)
-scale0 = torch.tensor(scale0_value, dtype=dtype)
-scale1 = torch.tensor(scale1_value, dtype=dtype)
+    s.backward()
 
-s0 = torch.relu((input_value @ w0.T + b0) * scale0)
-s1 = torch.relu((s0 @ w1.T + b1) * scale1)
-s = s1.sum()
-
-s.backward()
-
-torch_res = input_value.grad.numpy()
-
-if (abs(res - torch_res) < 1e-5).all():
-    print("# verify: PASSED")
-else:
-    print("# verify: FAILED")
-    print("Result:", res)
-    print("PyTorch Result:", torch_res)
-
+    return input_value.grad.numpy()
---
+++ tests/derivative/test_double_leyer.py
@@ -0,0 +1,10 @@
+from double_layer import *
+
+def test_single_layer():
+    inputs = generate_inputs()
+    weights = generate_weights()
+
+    nngen_res = nngen_gradient(inputs, weights)
+    torch_res = torch_gradient(inputs, weights)
+
+    assert (abs(nngen_res - torch_res) < 1e-5).all()
