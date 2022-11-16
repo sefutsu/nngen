c8b217083696aca3c933d386f2a736f1a04b9b9b
sefutsu <sefutsu.r@gmail.com>
2022-11-16 22:42:13 +0900

    pg_dtype is same as input dtype

--- nngen/verify/derivative/matmul.py
+++ nngen/verify/derivative/matmul.py
@@ -10,10 +10,9 @@ import nngen as ng
 def matmul(grad_output, a, b, deriv_by_a=True,
         saved_tensors=None, scale=None,
         transposed_a=False, transposed_b=False,
-        rshift_mul=None, rshift_sum=None, rshift_aaa=None,
-        a_dtype=None, b_dtype=None, scale_dtype=None, pg_dtype=None,
-        asymmetric_clip=False,
-        act_func=None, dtype=None,
+        a_dtype=None, b_dtype=None, scale_dtype=None,
+        mul_dtype=None, sum_dtype=None, dtype=None,
+        asymmetric_clip=False, act_func=None,
         **kwargs):
 
     c_shape = grad_output.shape
@@ -36,12 +35,11 @@ def matmul(grad_output, a, b, deriv_by_a=True,
         act_deriv_method = act_func.get_deriv_method()
         grad_output = act_deriv_method(grad_output, input_of_act_func)
 
-    if pg_dtype is None:
-        pg_dtype = ng.int32
-
+    if dtype is None:
+        dtype = ng.int32
     scale_point = 0 if scale_dtype is None else scale_dtype.point
-    scl_point = max(pg_dtype.point, scale_point)
-    scl_shift = min(pg_dtype.point, scl_point)
+    scl_point = max(dtype.point, scale_point)
+    scl_shift = min(dtype.point, scl_point)
     shifted_scale = np.right_shift(scale, scl_shift)
 
     grad_output = grad_output * shifted_scale[None]
@@ -49,58 +47,17 @@ def matmul(grad_output, a, b, deriv_by_a=True,
     if deriv_by_a:
         grad_output = matmul_forward(grad_output, b,
                     transposed_a=False, transposed_b=not transposed_b,
-                    a_dtype=pg_dtype, b_dtype=b_dtype, dtype=ng.int64)
+                    a_dtype=dtype, b_dtype=b_dtype, dtype=ng.int64)
         if transposed_a:
             grad_output = grad_output.transpose()
         if dtype is None: dtype = a_dtype
     else:
         grad_output = matmul_forward(a, grad_output,
                     transposed_a=not transposed_a, transposed_b=False,
-                    a_dtype=a_dtype, b_dtype=pg_dtype, dtype=ng.int64)
+                    a_dtype=a_dtype, b_dtype=dtype, dtype=ng.int64)
         if transposed_b:
             grad_output = grad_output.transpose()
-        if dtype is None: dtype = b_dtype
-    if dtype is None: dtype = ng.int32
-
-    c_shape = grad_output.shape
-    if rshift_mul is None:
-        rshift_mul = np.zeros([c_shape[-1]], dtype=np.int64)
-    elif not isinstance(rshift_mul, np.ndarray):
-        new_rshift_mul = np.zeros([c_shape[-1]], dtype=np.int64)
-        for i in range(new_rshift_mul.shape[-1]):
-            new_rshift_mul[i] = rshift_mul
-        rshift_mul = new_rshift_mul
-    elif len(rshift_mul.shape) == 1 and rshift_mul.shape[0] == 1:
-        new_rshift_mul = np.zeros([c_shape[-1]], dtype=np.int64)
-        for i in range(new_rshift_mul.shape[-1]):
-            new_rshift_mul[i] = rshift_mul[0]
-        rshift_mul = new_rshift_mul
-
-    if rshift_sum is None:
-        rshift_sum = np.zeros([c_shape[-1]], dtype=np.int64)
-    elif not isinstance(rshift_sum, np.ndarray):
-        new_rshift_sum = np.zeros([c_shape[-1]], dtype=np.int64)
-        for i in range(new_rshift_sum.shape[-1]):
-            new_rshift_sum[i] = rshift_sum
-        rshift_sum = new_rshift_sum
-    elif len(rshift_sum.shape) == 1 and rshift_sum.shape[0] == 1:
-        new_rshift_sum = np.zeros([c_shape[-1]], dtype=np.int64)
-        for i in range(new_rshift_sum.shape[-1]):
-            new_rshift_sum[i] = rshift_sum[0]
-        rshift_sum = new_rshift_sum
-
-    if rshift_aaa is None:
-        rshift_aaa = np.zeros([c_shape[-1]], dtype=np.int64)
-    elif not isinstance(rshift_aaa, np.ndarray):
-        new_rshift_out = np.zeros([c_shape[-1]], dtype=np.int64)
-        for i in range(new_rshift_out.shape[-1]):
-            new_rshift_out[i] = rshift_out
-        rshift_out = new_rshift_out
-    elif len(rshift_out.shape) == 1 and rshift_out.shape[0] == 1:
-        new_rshift_out = np.zeros([c_shape[-1]], dtype=np.int64)
-        for i in range(new_rshift_out.shape[-1]):
-            new_rshift_out[i] = rshift_out[0]
-        rshift_aaa = new_rshift_out
+    ret_dtype = a_dtype if deriv_by_a else b_dtype
 
     # dynamic quantization
     max_absolute_value = np.abs(grad_output).max()
@@ -108,20 +65,20 @@ def matmul(grad_output, a, b, deriv_by_a=True,
         highest_bit = np.floor(np.log2(max_absolute_value)).astype(np.int8)
     else:
         highest_bit = 0
-    available_highest_bit = dtype.width - 2
+    available_highest_bit = ret_dtype.width - 2
     rshift = max(0, highest_bit - available_highest_bit)
     if rshift > 0:
         grad_output += 1 << (rshift - 1)
         grad_output >>= rshift
 
-    p_th = (1 << (dtype.width - 1)) - 1
+    p_th = (1 << (ret_dtype.width - 1)) - 1
     if asymmetric_clip:
         n_th = -1 * p_th - 1
     else:
         n_th = -1 * p_th
 
-    p_th = p_th >> dtype.point
-    n_th = n_th >> dtype.point
+    p_th = p_th >> ret_dtype.point
+    n_th = n_th >> ret_dtype.point
 
     grad_output = np.where(grad_output > p_th, p_th, 
         np.where(grad_output < n_th, n_th, grad_output))
--- nngen/verify/derivative/relu.py
+++ nngen/verify/derivative/relu.py
@@ -4,9 +4,12 @@ from __future__ import division
 
 import numpy as np
 
-def relu(grad_output, features, dtype=None, pg_dtype=None):
-
-    rshift_out = 0 if dtype is None or pg_dtype is None else pg_dtype.point - dtype.point
+def relu(grad_output, features, dtype=None, features_dtype=None):
+    # grad_output: dtype
+    # return: features_dtype
+    grad_point = 0 if dtype is None else dtype.point
+    features_point = 0 if features_dtype is None else features_dtype.point
+    rshift_out = grad_point - features_point
     grad_output = grad_output >> rshift_out if rshift_out >= 0 \
                         else grad_output << (-rshift_out)
 
