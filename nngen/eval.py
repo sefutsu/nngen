9e83b6e52a4c1d4bceacefa7e9107ad37480c2f7
sefutsu <sefutsu.r@gmail.com>
2023-04-28 17:35:06 +0900

    changes for backward

--- nngen/eval.py
+++ nngen/eval.py
@@ -2,7 +2,12 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
+import numpy as np
 
 def eval(objs, **input_dict):
     memo = {}
     return [obj.eval(memo, input_dict) for obj in objs]
+
+def backward(objs):
+    for obj in objs:
+        obj.backward(np.array(1.0), 1.0)
